613a1c12859950a7f531296f790cdd591dc78b98
codewithxavi <javieradrian.it@gmail.com>
2023-02-06 14:40:57 +0100

    first javascript lessons

---
+++ JS/20230206-variables.js
@@ -0,0 +1,15 @@
+var alert1 = 5;
+// permitido
+var alert1 = '';
+let alert2 = 5;
+// no permitido porque ya se declaró antes
+// let alert2 = 5;
+alert2 = 15;
+const alert3 = 13;
+
+
+console.log(alert1, alert2, alert3);
+
+const nombre = prompt("Escriba su nombre", "Escriba aquí");
+console.log(nombre);
+
---
+++ JS/primer.js
@@ -0,0 +1,20 @@
+// hola
+console.log('hola');
+
+
+// en un lugar de la mancha
+console.log('en', 'un', 'lugar', 'de', 'la', 'Mancha');
+console.info('Info');
+console.warn('Warning');
+console.error('Error');
+
+document.getElementById("demo").innerHTML = "hola"
+
+const nombre = prompt("Escriba su nombre", "Escriba aquí");
+console.log(nombre);
+
+const conf = confirm("Sí o no");
+// true o fase
+console.log(conf);
+
+console.log((!+[]+[]+![]).length);
